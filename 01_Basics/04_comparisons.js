a60ad85e627be61368e84301128c89499351d208
Anurag Agrawal <145379061+anuragag26@users.noreply.github.com>
2025-06-24 06:22:33 +0000

    comparisons

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,26 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log(null>0); // comparisons convert null to a number, treating it as 0
+console.log(null==0); //equality check
+console.log(null>=0); // comparisons convert null to a number, treating it as 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === (strict check)
+
+console.log("2" === 2);  //checks data type also
+
+
+
+
+
