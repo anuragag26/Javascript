becfc03c0452b35cfe888ee8054e6317729836b7
Anurag Agrawal <145379061+anuragag26@users.noreply.github.com>
2025-06-24 06:49:15 +0000

    dataType Summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,53 @@
+/* *******
+The way data is stored in the memory
+and the way it is accessed from the 
+memory , based on that the data is divided 
+into two types ->
+1. Primitive
+2. Non-Primitive
+********** */
+
+// Primitive
+// Call By Value
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 23432545453434343433424n   // bigInt
+
+
+// Non-Primitive (Reference)
+// Call By reference
+//  Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"]
+
+let myObj = {
+    name: "Anurag",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+
+// All Non-primitve data type ka return type Object aata he but function ka
+// (object function) aata he
+
+
+// Javascript is Dynamically typed language means we don't have to define
+// data type while declaring a variable
+
+
